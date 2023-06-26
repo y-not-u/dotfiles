87ab9c8f3b7c3807ce65a20596db2db1bc1ca830
y-not-u <102951988+y-not-u@users.noreply.github.com>
2023-06-26 23:18:42 +0800

    feat(nvim): cursor blink

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,2 +1,7 @@
 vim.opt.termguicolors = true
 vim.opt.guifont = "Hack Nerd Font"
+vim.o.guicursor = table.concat({
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")
