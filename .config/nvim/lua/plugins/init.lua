3e807fcdc02081da3fedf098b6731b5336289db5
y-not-u <102951988+y-not-u@users.noreply.github.com>
2023-07-05 11:54:19 +0800

    refactor(nvim): update the flash config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -647,25 +647,15 @@ require("lazy").setup({
         "s",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump()
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+          })
         end,
         desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-        desc = "Flash Treesitter Search",
       },
     },
   }
