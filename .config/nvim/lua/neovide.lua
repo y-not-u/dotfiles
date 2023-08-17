a758faaf720fbbebcbbc8c544304efc62d6c35bc
y-not-u <102951988+y-not-u@users.noreply.github.com>
2023-08-17 16:02:52 +0800

    fix: paste in neovide

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -1,1 +1,15 @@
+local opts = {
+  silent = true,
+  noremap = true
+}
+local map = vim.api.nvim_set_keymap
+
+vim.opt.guifont = "Hack Nerd Font:h18"
 vim.g.neovide_transparency = 0.8
+vim.g.neovide_remember_window_size = true
+
+vim.g.neovide_input_use_logo = 1
+map("", "<D-v>", "\"+p<CR>", opts)
+map("!", "<D-v>", "<C-R>+", opts)
+map("t", "<D-v>", "<C-R>+", opts)
+map("v", "<D-c>", "\"+y<CR>", opts)
