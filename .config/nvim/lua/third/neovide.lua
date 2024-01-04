22c40c9fe797803a3115a5130ff2e837e2219106
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-01-04 22:30:29 +0800

    feat(nvim): scale font size in neovide

--- .config/nvim/lua/third/neovide.lua
+++ .config/nvim/lua/third/neovide.lua
@@ -15,3 +15,10 @@ map("", "<D-v>", "\"+p<CR>", opts)
 map("!", "<D-v>", "<C-R>+", opts)
 map("t", "<D-v>", "<C-R>+", opts)
 map("v", "<D-c>", "\"+y<CR>", opts)
+
+-- scale font size at runtime
+vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+  { silent = true })
+vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+  { silent = true })
+vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
