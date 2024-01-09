01a7544c07cb555f88e9ddcdcc51cf6af3dce0d8
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-01-10 02:40:59 +0800

    feat(nvim): new keymaps

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -2,6 +2,14 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 -- Lazy
 map('n', '<leader>l', '<cmd>Lazy<CR>', opts)
 
@@ -20,8 +28,11 @@ map('n', '<leader>r', '<cmd>so %<CR>', opts)
 -- buffer
 map('n', '[b', '<cmd>bp<CR>', term_opts)
 map('n', ']b', '<cmd>bn<CR>', term_opts)
+map('n', '<S-h>', '<cmd>bp<CR>', term_opts)
+map('n', '<S-l>', '<cmd>bn<CR>', term_opts)
 map('n', '[[', '<cmd>bp<CR>', term_opts)
 map('n', ']]', '<cmd>bn<CR>', term_opts)
+map("n", "<leader>bb", "<cmd>e #<cr>", term_opts)
 
 -- Resize with arrows
 map("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -34,6 +45,16 @@ map("c", "w!!", "w !sudo tee % >/dev/null<CR>:e!<CR><CR>", opts)
 
 -- jk to esc
 map("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+-- Clear search with <esc>
+map("n", "<esc>", "<cmd>noh<cr><esc>", opts)
+
+-- Visual Block --
+-- Move text up and down
+map("x", "J", ":move '>+1<CR>gv-gv", opts)
+map("x", "K", ":move '<-2<CR>gv-gv", opts)
+map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- indent
 map("v", "<", "<gv", opts)
