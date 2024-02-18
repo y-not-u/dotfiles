29b74d722aa34654f3246dddc15d8c1799e501bc
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-02-18 13:20:25 +0800

    chore(nvim): update

--- .config/nvim/lua/plugins/utilities.lua
+++ .config/nvim/lua/plugins/utilities.lua
@@ -148,6 +148,11 @@ return {
     "okuuva/auto-save.nvim",
     cmd = "ASToggle",                         -- optional for lazy loading on command
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-    opts = {},
+    opts = {
+      execution_message = {
+        enabled = false
+      },
+      debounce_delay = 5000,
+    },
   },
 }
