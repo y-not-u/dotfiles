d34c50a7f18edc66e1bf3045b97e3e43f16d69dc
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-02-19 17:45:47 +0800

    feat(hyprland): add hdrop script

--- .config/nvim/lua/plugins/files.lua
+++ .config/nvim/lua/plugins/files.lua
@@ -58,20 +58,6 @@ return {
     },
   },
 
-  -- bookmarks
-  {
-    'crusj/bookmarks.nvim',
-    keys = {
-      { "<tab><tab>", mode = { "n" } },
-    },
-    branch = 'main',
-    dependencies = { 'nvim-web-devicons' },
-    config = function()
-      require("bookmarks").setup()
-      require("telescope").load_extension("bookmarks")
-    end
-  },
-
   -- project
   {
     "coffebar/neovim-project",
