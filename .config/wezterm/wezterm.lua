6a97b0554157eb50953815250c29e7a94ad823c1
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-02-20 09:18:47 +0800

    chore: set wezterm background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,7 +34,7 @@ return {
 
   -- color scheme
   term = "xterm-256color",
-  color_scheme = 'DanQing',
+  color_scheme = 'tokyonight',
 
   -- font
   font = wezterm.font_with_fallback({
@@ -57,66 +57,27 @@ return {
 
   -- window
   window_background_opacity = 0.85,
-  macos_window_background_blur = 10,
+  macos_window_background_blur = 20,
   native_macos_fullscreen_mode = true,
   window_close_confirmation = "NeverPrompt",
   window_padding = {
-    left = 8,
-    right = 8,
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
   },
-  window_background_gradient = {
-    -- Can be "Vertical" or "Horizontal".  Specifies the direction
-    -- in which the color gradient varies.  The default is "Horizontal",
-    -- with the gradient going from left-to-right.
-    -- Linear and Radial gradients are also supported; see the other
-    -- examples below
-    orientation = 'Vertical',
-
-    -- Specifies the set of colors that are interpolated in the gradient.
-    -- Accepts CSS style color specs, from named colors, through rgb
-    -- strings and more
-    colors = {
-      '#0f0c29',
-      '#302b63',
-      '#24243e',
+  background = {
+    {
+      source = {
+        File = '/home/egg/Pictures/Wallpapers/archlinux.jpg',
+      },
+      opacity = 1.0,
+      repeat_x = 'NoRepeat',
+      repeat_y = 'NoRepeat',
+      vertical_align = 'Middle',
+      horizontal_align = 'Center',
     },
-
-    -- Instead of specifying `colors`, you can use one of a number of
-    -- predefined, preset gradients.
-    -- A list of presets is shown in a section below.
-    -- preset = "Warm",
-
-    -- Specifies the interpolation style to be used.
-    -- "Linear", "Basis" and "CatmullRom" as supported.
-    -- The default is "Linear".
-    interpolation = 'Linear',
-
-    -- How the colors are blended in the gradient.
-    -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
-    -- The default is "Rgb".
-    blend = 'Rgb',
-
-    -- To avoid vertical color banding for horizontal gradients, the
-    -- gradient position is randomly shifted by up to the `noise` value
-    -- for each pixel.
-    -- Smaller values, or 0, will make bands more prominent.
-    -- The default value is 64 which gives decent looking results
-    -- on a retina macbook pro display.
-    -- noise = 64,
-
-    -- By default, the gradient smoothly transitions between the colors.
-    -- You can adjust the sharpness by specifying the segment_size and
-    -- segment_smoothness parameters.
-    -- segment_size configures how many segments are present.
-    -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
-    -- 1.0 is a soft edge.
-
-    -- segment_size = 11,
-    -- segment_smoothness = 0.0,
   },
-
   inactive_pane_hsb = {
     saturation = 0.8,
     brightness = 0.7
@@ -124,7 +85,7 @@ return {
 
   use_dead_keys = false,
   scrollback_lines = 5000,
-  enable_scroll_bar = true,
+  enable_scroll_bar = false,
 
   -- keys
   keys = {
