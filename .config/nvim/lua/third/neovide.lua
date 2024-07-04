bb03f983d0c6c0c7d50a324968ce1cb86c9cb805
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-07-04 12:38:14 +0800

    refactor: update nvim

--- .config/nvim/lua/third/neovide.lua
+++ .config/nvim/lua/third/neovide.lua
@@ -17,7 +17,7 @@ map("t", "<D-v>", "<C-R>+", opts)
 map("v", "<D-c>", "\"+y<CR>", opts)
 
 -- scale font size at runtime
-vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+vim.api.nvim_set_keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
   { silent = true })
 vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
   { silent = true })
