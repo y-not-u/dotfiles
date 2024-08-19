16a5ab4221d31492343d3e09d7917019bf02b2e4
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-08-19 23:10:08 +0800

    chore(nvim): update setups

--- .config/nvim/lua/plugins/AI.lua
+++ .config/nvim/lua/plugins/AI.lua
@@ -19,4 +19,29 @@ return {
       })
     end,
   },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    opts = {
+      provider = "openai",
+      openai = {
+        endpoint = "https://burn.hair",
+        model = "gpt-4o",
+        temperature = 0,
+        max_tokens = 4096,
+      },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "grapp-dev/nui-components.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim"
+        }
+      },
+    },
+  }
 }
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,10 @@
 return {
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
   {
-    "OXY2DEV/markview.nvim",
-    -- lazy = false, -- Recommended
-    ft = "markdown", -- If you decide to lazy-load anyway
-
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    }
-  }
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
 }
