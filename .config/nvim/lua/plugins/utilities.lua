8fce5705a85e56115810e398214779265fe76056
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-08-22 23:12:33 +0800

    chore(nvim): remove open-url

--- .config/nvim/lua/plugins/utilities.lua
+++ .config/nvim/lua/plugins/utilities.lua
@@ -68,21 +68,6 @@ return {
     },
   },
 
-  -- url open
-  {
-    "sontungexpt/url-open",
-    branch = "mini",
-    event = "VeryLazy",
-    cmd = "URLOpenUnderCursor",
-    config = function()
-      local status_ok, url_open = pcall(require, "url-open")
-      if not status_ok then
-        return
-      end
-      url_open.setup({})
-    end,
-  },
-
   -- harpoon
   {
     "ThePrimeagen/harpoon",
