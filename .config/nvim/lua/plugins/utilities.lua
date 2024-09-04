7899527af75e391eb5d0b1ec27031d583b65369e
y-not-u <voganwong@hotmail.com>
2024-09-04 14:21:10 +0800

    chore(zed): update setups

--- .config/nvim/lua/plugins/utilities.lua
+++ .config/nvim/lua/plugins/utilities.lua
@@ -143,5 +143,26 @@ return {
     'mei28/luminate.nvim',
     event = { 'VeryLazy' },
     config = true
-  }
+  },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    keys = {
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      }
+    },
+  },
 }
