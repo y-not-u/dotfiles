488b2fcd288c071de3396f3768f7c9c464bc6439
y-not-u <voganwong@hotmail.com>
2024-11-12 09:25:13 +0800

    chore: update hyprland config for 0.45

--- .config/nvim/lua/plugins/AI.lua
+++ .config/nvim/lua/plugins/AI.lua
@@ -1,5 +1,3 @@
-local window = require "utils.window"
-
 return {
   {
     'Exafunction/codeium.vim',
@@ -17,36 +15,4 @@ return {
       })
     end,
   },
-  {
-    "yetone/avante.nvim",
-    -- event = "VeryLazy",
-    version = false,
-    keys = {
-      { "<leader>aa", "<cmd>AvanteToggle<CR>", desc = "Toggle Avante" },
-    },
-    opts = {
-      provider = "openai",
-      openai = {
-        endpoint = "https://burn.hair/v1",
-        model = "gpt-4o",
-        timeout = 30000,
-        temperature = 0,
-        max_tokens = 4096,
-      },
-      windows = {
-        width = window.side_width,
-        sidebar_header = {
-          align = "right", -- left, center, right for title
-          rounded = false,
-        },
-      }
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  }
 }
