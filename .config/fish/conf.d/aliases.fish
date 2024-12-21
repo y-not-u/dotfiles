da1215436fdde59b51fd0f6c35a1678c1db22490
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-12-21 23:55:07 +0800

    chore: update fish config

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -5,16 +5,12 @@ alias cp='xcp'
 alias cd='z'
 alias pn="pnpm"
 alias pnpx="pnpm dlx"
-alias s="proxychains4"
 alias ra="ranger"
 alias jo="joshuto"
 alias ze="zellij"
 alias lg='lazygit'
-alias ld='DOCKER_HOST=unix:///run/user/1000/podman/podman.sock lazydocker'
 alias mf='musicfox'
-alias zh="trans :zh-cn"
-alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
-alias rm='rmtrash'
+alias c="clear"
 
 function l
     ls -lh $argv
---
+++ .config/fish/config_linux.fish
@@ -0,0 +1,4 @@
+alias rm='rmtrash'
+alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+alias zh="trans :zh-cn"
+alias s="proxychains4"
